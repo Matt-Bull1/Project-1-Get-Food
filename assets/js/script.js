2b7b5b6b1f68d59f4c2044ff42528efffbdd4f7e
Nestor Montanez <nestormontanez@Nestors-MacBook-Pro.local>
2023-03-12 21:56:34 -0400

    got history log to work and clear button

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,21 +3,23 @@ var searchBtn = document.getElementById("search-btn");
 var flipBtn = document.getElementById("flipBtn");
 var returnBtn = document.getElementById("returnBtn");
 var url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
-var nameResult = document.getElementById("foodName")
-var displayIng = document.getElementById("displayIng")
-var displayIMG = document.getElementById("displayIMG") 
-var ingredientsEl = document.getElementById("ingredientList")
-var userInp = document.getElementById("user-inp")
+var nameResult = document.getElementById("foodName");
+var displayIng = document.getElementById("displayIng");
+var displayIMG = document.getElementById("displayIMG"); 
+var ingredientsEl = document.getElementById("ingredientList");
+var userInp = document.getElementById("user-inp");
+var lastRecipe = "";
+var currentRecipe = "";
 
 flipBtn.addEventListener("click", () => {
     var flipCard = document.getElementById("nutrition")
-    console.log(flipCard)
+    // console.log(flipCard)
     flipCard.classList.add("flip")
 } )
 
 returnBtn.addEventListener("click", () => {
     var flipCard = document.getElementById("nutrition")
-    console.log(flipCard)
+    // console.log(flipCard)
     flipCard.classList.remove("flip")
 } )
 
@@ -29,30 +31,32 @@ userInp.addEventListener("keypress", (e) => {
     if (e.key === "Enter") {
         e.preventDefault();
         getRecipe();
-        console.log()
+        // console.log()
     }
 
 })
 
 function getRecipe () {
     var userInpVal = userInp.value
-        console.log(userInp)
+
+        // console.log(userInp)
     if (userInpVal.length == 0) {
-      result.innerHTML = `<h3>Input Field Cannot Be Empty</h3>`;
+      result.innerHTML = `<h3>Search Field Cannot Be Empty</h3>`;
     } else {
     fetch(url + userInpVal)
     .then((response) => response.json())
     .then((data) => {
-        console.log(data)
-        console.log(data.meals.length);
+        saveRecipe(userInpVal)
+        // console.log(data)
+        // console.log(data.meals.length);
         var arrayLength = data.meals.length;
         var pickMeal = Math.floor(Math.random() * arrayLength)
-        console.log(pickMeal)
+        // console.log(pickMeal)
         var randomMeal = data.meals[pickMeal]
-        console.log(randomMeal)
-        console.log(randomMeal.strMealThumb)
-        console.log(randomMeal.strMeal)
-        console.log(randomMeal.strInstructions)
+        // console.log(randomMeal)
+        // console.log(randomMeal.strMealThumb)
+        // console.log(randomMeal.strMeal)
+        // console.log(randomMeal.strInstructions)
         var cookIntruct = randomMeal.strInstructions
         var foodIMG = randomMeal.strMealThumb
         var foodName = randomMeal.strMeal
@@ -73,7 +77,7 @@ function getRecipe () {
         ingredientsEl.innerHTML = ingredientList.join("<br/>"
         )
 
-        console.log(ingredientList)
+        // console.log(ingredientList)
 displayIMG.innerHTML = "<img src =" + foodIMG + ">"
     })}
 }
@@ -82,75 +86,72 @@ displayIMG.innerHTML = "<img src =" + foodIMG + ">"
 
 
 
-//    var saveRecipe = (newRecipe) => {
-//     let recipeExists = false;
-//     for (let i = 0; i < localStorage.length; i++) {
-//         if (localStorage["recipes" + i] === newRecipe) {
-//             recipeExists = true;
-//             break;
-//         }
-//     }
-//     if (recipeExists === false) {
-//         localStorage.setItem('recipes' + localStorage.length, newRecipe);
-//     }
-// }
+   var saveRecipe = (newRecipe) => {
+    console.log(newRecipe)
+    let recipeExists = false;
+    for (let i = 0; i < localStorage.length; i++) {
+        if (localStorage["recipes" + i] === newRecipe) {
+            recipeExists = true;
+            break;
+        }
+    }
+    if (recipeExists === false) {
+        localStorage.setItem('recipes' + localStorage.length, newRecipe);
+    }
+    renderRecipes()
+}
 
 
-// var renderRecipes = () => {
-//     $('#recipe-results').empty();
-//     if (localStorage.length===0){
-//         if (lastRecipe){
-//             $('#search-recipe').attr("value", lastRecipe);
-//         } else {
-//             $('#search-recipe').attr("value", "Beef");
-//         }
-//     } else {
-//         let lastRecipeKey="recipes"+(localStorage.length-1);
-//         lastRecipe=localStorage.getItem(lastRecipeKey);
-//         $('#search-recipe').attr("value", lastRecipe);
-//         for (let i = 0; i < localStorage.length; i++) {
-//             let recipe = localStorage.getItem("recipes" + i);
-//             let recipeEl;
-//             if (currentRecipe===""){
-//                 currentRecipe=lastRecipe;
-//             }
+var renderRecipes = () => {
+    $('#recipe-results').empty();
+    if (localStorage.length===0){
+        if (lastRecipe){
+            $('#search-recipe').attr("value", lastRecipe);
+        } else {
+            $('#search-recipe').attr("value", "Beef");
+        }
+    } else {
+        let lastRecipeKey="recipes"+(localStorage.length-1);
+        lastRecipeKey=localStorage.getItem(lastRecipeKey);
+        $('#search-recipe').attr("value", lastRecipe);
+        for (let i = 0; i < localStorage.length; i++) {
+            let recipe = localStorage.getItem("recipes" + i);
+            let recipeEl;
+            if (currentRecipe===""){
+                currentRecipe=lastRecipe;
+            }
         
-//             if (recipe === currentRecipe) {
-//                 recipeEl = `<button type="button" class="list-group-item list-group-item-action active">${city}</button></li>`;
-//             } else {
-//                 recipeEl = `<button type="button" class="list-group-item list-group-item-action">${city}</button></li>`;
-//             } 
+            if (recipe === currentRecipe) {
+                recipeEl = `<li><button class="button is-fullwidth active" type="button">${recipe}</button></li>`;
+            } else {
+                recipeEl = `<li><button class="button is-fullwidth" type="button">${recipe}</button></li>`;
+            } 
         
-//             $('#recipe-results').prepend(cityEl);
-//         }
+            $('#recipe-results').prepend(recipeEl);
+        }
         
-//         if (localStorage.length>0){
-//             $('#clear-storage').html($('<a id="clear-storage" href="#">clear</a>'));
-//         } else {
-//             $('#clear-storage').html('');
-//         }
-//     }
+        if (localStorage.length>0){
+            $('#clear-storage').html($('<a id="clear-storage" href="#">clear</a>'));
+        } else {
+            $('#clear-storage').html('');
+        }
+    }
     
-// }
+}
 
-// $('#search-button').on("click", (event) => {
-// event.preventDefault();
-// currentRecipe = $('#search-Recipe').val();
-// getCurrentConditions(event);
-// });
+$('#recipe-results').on("click", (event) => {
+    event.preventDefault();
+    $('#search-recipe').val(event.target.textContent);
+    $('#user-inp').val(event.target.textContent);
+    currentRecipe=$('#search-recipe').val();
+    getRecipe();
+});
 
-// $('#recipe-results').on("click", (event) => {
-//     event.preventDefault();
-//     $('#search-recipe').val(event.target.textContent);
-//     currentRecipe=$('#search-recipe').val();
-//     getCurrentConditions(event);
-// });
+$("#clear-storage").on("click", (event) => {
+    localStorage.clear();
+    renderRecipes();
+});
 
-// $("#clear-storage").on("click", (event) => {
-//     localStorage.clear();
-//     renderRecipes();
-// });
+renderRecipes();
 
-// renderRecipes();
 
-// getCurrentConditions();
